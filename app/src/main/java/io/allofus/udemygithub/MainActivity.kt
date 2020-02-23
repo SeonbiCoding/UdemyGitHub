e0a7e4d18e78e7da3d3308deddc77f561dd0766d
ParkSeonbi <55050662+ParkSeonbi@users.noreply.github.com>
2020-02-23 13:52:36 +0900

    Update two files one in the xml, and the other is kt file

--- app/src/main/java/io/allofus/udemygithub/MainActivity.kt
+++ app/src/main/java/io/allofus/udemygithub/MainActivity.kt
@@ -2,11 +2,14 @@ package io.allofus.udemygithub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val text = findViewById<TextView>(R.id.textView2)
     }
 }
